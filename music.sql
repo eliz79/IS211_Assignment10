caef7824db421c1e7bd38a110188d99d07566f74
Erica Liz <erica.liz@spsmail.cuny.edu>
2015-11-10 02:32:28 +0000

    adding assignment 10

---
+++ music.sql
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""My music.sql query"""
+
+#will create 3 tables for the music query
+CREATE TABLE artist (id INTEGER PRIMARY KEY, artist TEXT);
+CREATE TABLE album (id INTEGER PRIMARY KEY, album TEXT, artist_id INTEGER);
+CREATE TABLE song (id INTEGER PRIMARY KEY, song TEXT, artist_id INTEGER,
+                   track_num INTEGER, track_sec INTEGER);
+
+#artist table - gives a column for an artist name
+#album table - gives album name, and artist
+#song table - gives song name, artist, track #, and seconds of track
