42a73030c6b94d84a48ce9eb5afec3e4d7319384
Erica Liz <erica.liz@spsmail.cuny.edu>
2015-12-20 01:29:09 +0000

    correct music

--- music.sql
+++ music.sql
@@ -5,7 +5,7 @@
 #will create 3 tables for the music query
 CREATE TABLE artist (id INTEGER PRIMARY KEY, artist TEXT);
 CREATE TABLE album (id INTEGER PRIMARY KEY, album TEXT, artist_id INTEGER);
-CREATE TABLE song (id INTEGER PRIMARY KEY, song TEXT, artist_id INTEGER,
+CREATE TABLE song (id INTEGER PRIMARY KEY, song TEXT, album_id INTEGER, artist_id INTEGER,
                    track_num INTEGER, track_sec INTEGER);
 
 #artist table - gives a column for an artist name
